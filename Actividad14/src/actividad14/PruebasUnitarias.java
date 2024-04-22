93c58bcca98cdba0ab3db5a4bda9cd4bb37a3818
alejandro <grajalesalejandro068@gmail.com>
2024-04-22 13:13:02 +0200

    pruebas unitarias alejo

--- Actividad14/src/actividad14/PruebasUnitarias.java
+++ Actividad14/src/actividad14/PruebasUnitarias.java
@@ -2,4 +2,28 @@ package actividad14;
 
 public class PruebasUnitarias {
 
+	/**
+	 * prueba 1 
+	 * 
+	 * caso1. verica si puedeDisparar devuelve true cuando el nuemero de balas es >0
+	 * 
+	 * si es asi llama al metodo disparar, la propiedad estaMuerto se establece a true y el numero de balas se reduce en 1
+	 
+	 * resultado esperado true;
+	 * 
+	 * 
+	 * 
+	 * 
+	 * Prueba 2
+	 
+	 * verifica si puedeDisparar devuleve false cuando el numero de balas es <=0
+	 
+	 * si es asi llama al metodo disparar, la propiedad estaMuerto se establece a false y el numero de balas no se reduce.
+	 
+	 * resultado esperado false;  
+	
+	 *
+	 *
+	 * 
+	 */
 }
