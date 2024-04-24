3a671934e71c6db0e28702a1fe05626845129955
alejandro <grajalesalejandro068@gmail.com>
2024-04-24 14:59:38 +0200

    pruebas unitarias

--- Actividad14/src/actividad14/PruebasUnitarias.java
+++ Actividad14/src/actividad14/PruebasUnitarias.java
@@ -2,34 +2,93 @@ package actividad14;
 
 public class PruebasUnitarias {
 
-	/**
+	/** clase soldado
+	 * 
+	 * METODO puedeDisparar()
 	 * prueba 1 
 	 * 
 	 * caso1. verica si puedeDisparar devuelve true cuando el nuemero de balas es >0
-	 * 
-	 * si es asi llama al metodo disparar, la propiedad estaMuerto se establece a true y el numero de balas se reduce en 1
-	 
+	 *
 	 * resultado esperado:
 
 	 this.numeroBalas = 8;
 	 * 		
 	  resultado esperado = true
 	 * 
-	 * 
-	 * 
-	 * 
+
 	 * Prueba 2
 	 
 	 * verifica si puedeDisparar devuleve false cuando el numero de balas es <=0
-	 
-	 * si es asi llama al metodo disparar, la propiedad estaMuerto se establece a false y el numero de balas no se reduce.
 	 
 	 * resultado esperado:  
 	
 	 *  this.numeroBalas = o;
 	 * 		
 	  resultado esperado = false
-	 *
+
+	 * METODO disparar()
+	 * verifica si dispara cuando el numero de balas es >=0, de ser asi dispara al objeto introducido por paramentro 
+	 * resultado esperado: true;
 	 * 
-	 */
+	 
+
+
+	 *  clase jugador 
+	 * 
+	 *METODO poner dorsal()
+
+	 Prueba1.
+	 * resultado esperado: 20;
+
+	 * 		int dorsal = 20;
+	 * 		
+	 * 
+	 * 		this.dorsal = 20;
+	 * 
+	
+	 * Prueba2. 
+	 *  resultado esperado: -1;
+	 * 		int dorsal = 40;
+	 * 		
+	 * 		 this.dorsal = -1;
+	 * 
+	 
+	 * Prueba3.
+	 * resultado esperado: -1;
+	 * 		int dorsal = 0;
+	 * 		
+	 * 		 this.dorsal = -1;
+	 * 
+	 
+	 
+	 * METODO estaExpulsado()
+	 * 
+	 * pruba1
+	 * resultado esperado: false;
+	 * 
+	 * numeroTarjetasAmariilas= 1;
+	 * 
+	 *		expulsado = false;
+
+	 *prueba 2
+
+	 * resultado esperado: true;
+	 * 
+	 *  numeroTarjetasAmarillas = 2;
+	 *  	expulsado=true;
+	 * 	
+	 * prueba 3
+	 
+	 * resultado esperado: true;
+
+	 *  numeroTarjetasRojas =1;
+	 * 		 expulsado = true;
+	 * 
+	 *  prueba 4
+	 *  resultado esperado: false;
+	 * 
+	 * numeroTarjeasRojas =0;
+	 *		 expulsado = false;
+	 * /	
+
 }
